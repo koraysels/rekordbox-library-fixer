a7163ebea232b0aea5623f8a8ff112271aba6394
Koray Sels <koray.sels@gmail.com>
2025-08-26 17:04:41 +0200

    accidentally deleted styling

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        rekordbox: {
+          purple: '#A855F7',
+          dark: '#18181B',
+          gray: '#27272A',
+          light: '#F4F4F5',
+        }
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [],
+}
