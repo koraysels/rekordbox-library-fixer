7003e9cd3c22d82e61779e794a405dd1c603ca9a
Koray Sels <koray.sels@gmail.com>
2025-08-27 09:35:40 +0200

    cleanup

--- src/renderer/components/ui/index.ts
+++ src/renderer/components/ui/index.ts
@@ -1,8 +1,6 @@
 // Barrel export for UI components
 export { AppHeader } from './AppHeader';
 export { NotificationToast } from './NotificationToast';
-export { NavigationTabs } from './NavigationTabs';
-export { NavigationTabsRouted } from './NavigationTabsRouted';
 export { EmptyLibraryState } from './EmptyLibraryState';
 export { AppFooter } from './AppFooter';
 export { SplashScreen } from './SplashScreen';
