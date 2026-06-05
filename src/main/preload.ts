29682206bd2c089387e0aade7a146f4a520a6384
Koray Sels <koray.sels@gmail.com>
2026-06-05 13:20:05 +0200

    feat: add file-exists IPC handler and checkFileAccessible preload

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -4,6 +4,7 @@ import { contextBridge, ipcRenderer } from 'electron';
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('electronAPI', {
   selectRekordboxXML: () => ipcRenderer.invoke('select-rekordbox-xml'),
+  checkFileAccessible: (path: string) => ipcRenderer.invoke('file-exists', path),
   selectFolder: () => ipcRenderer.invoke('select-folder'),
   parseRekordboxLibrary: (xmlPath: string) =>
     ipcRenderer.invoke('parse-rekordbox-library', xmlPath),
